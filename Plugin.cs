96cea7df4753c573f5713f2a3653469ae11fe817
Joshua Pollick <39402728+Hypersycos@users.noreply.github.com>
2023-03-03 17:28:23 +0000

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace StaminaChange
+{
+    [BepInPlugin("hypersycos.plugins.arkshot.staminaplus", "Stamina Plus", "0.1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
